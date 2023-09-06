cc220869bd788f73b2be9cddeaf0dbd3ef22b16f
inf-sid <sidnei.simeao@vilanova.com.br>
2023-09-06 15:40:31 -0300

    Teste ajuste

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ require __DIR__ . '/vendor/autoload.php';
 
 use CodeWars\Kyu7\CreatePhoneNumber;
 
-echo CreatePhoneNumber::run([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+echo CreatePhoneNumber::run([9, 9, 9, 4, 5, 6, 7, 8, 9, 0]);
